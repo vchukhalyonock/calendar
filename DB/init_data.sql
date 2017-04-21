7c1b393b5e3594fee9b26d7e24047f71bc45db6e
Vladimir Chukhalyonock <vchukhalyonock@gmail.com>
2017-04-22 00:04:53 +0300

    Add initial admin user

---
+++ DB/init_data.sql
@@ -0,0 +1,2 @@
+INSERT INTO `users` (`id`, `email`, `password`, `name`, `surname`, `type`, `registrationCode`) VALUES
+  (NULL, 'admin@example.com', '21232f297a57a5a743894a0e4a801fc3', 'Admin', 'Admin', 'admin', NULL);
